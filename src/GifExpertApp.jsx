add17b98e4df5eedfea56d440d61938775f62f0d
Gabriel Vega <gvegag@miumg.edu.gt>
2023-02-09 20:37:54 -0600

    feat> adding items with useState hook

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
 
 export const GifExpertApp=()=>{
+    
+    //Hook de react para mantener el estado
+    const [categories, setCategories] = useState(['One Punch','Dragon Ball']);
+    console.log(categories)
+
     return (
         <>
-            <h1>GifExpertApp</h1>
+            {/*Titulo */}
+            <h1>GIFExpertApp</h1>
+
+            {/*Input */}
+
+            {/*Listado de Gif */}
+            <ol>
+                {categories.map(category=>{
+                    return <li key={category}>{category}</li>
+                })}
+                <li></li>
+            </ol>
+                {/*Gif item */}
         </>
     )
 }
